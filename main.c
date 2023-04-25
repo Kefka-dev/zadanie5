202dbab602195fa36740f14f1f39c43f36672d5e
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-04-26 00:29:25 +0200

    funkcia na kontrolu stavu E1

--- main.c
+++ main.c
@@ -31,14 +31,52 @@ void initMenu(RESTAURANT *restaurant)
 }
 
 int main(int argc, char* argv[])
+int getRestNames(const char* dbFileName, FILE_NAME **storeHere)
 {
-    RESTAURANT *db;
+    FILE *db, *tmp;
+    db = fopen(dbFileName, "r");
 
-    printf("%s\n", argv[1]);
-    for (int i = 0; i < strlen(argv[1]); i++)
+    char buffer[MAX_NAME];
+    char path[MAX_NAME+PATH_LEN]; 
+    strcpy(path, PATH);
+    
+    int SizeRestaurantFiles= INITIAL_MALLOC_SIZE;
+    (*storeHere) = (FILE_NAME *)malloc(SizeRestaurantFiles*sizeof(FILE_NAME));
+    int restCount=0;
+    
+    while (fgets(buffer, MAX_NAME, db) != NULL)
     {
-        printf("%c\n", argv[1][i]);
+        if(buffer[strlen(buffer)-1] == '\n')  
+            buffer[strlen(buffer)-1] = '\0';
+        strcat(path, buffer);
+        //printf("%s\n", path);
+        tmp = fopen(path, "r"); 
+        //
+        if ( tmp == NULL)
+        {
+            //printf("file cislo %2d sa nepodarilo otvorit\n", restCount);
+            fprintf(stderr, "E1 %s\n", buffer);
+            path[PATH_LEN-1] = '\0';
+            continue;
+        }
+        else
+        {
+            if (restCount == SizeRestaurantFiles)
+            {
+                SizeRestaurantFiles = SizeRestaurantFiles + 16;
+                (*storeHere) = (FILE_NAME*)realloc((*storeHere), SizeRestaurantFiles*sizeof(FILE_NAME));
+            }
+            
+            strcpy((*storeHere)[restCount].name, path);
+            //printf("file cislo %2d otvoreny\n", restCount);
+            fclose(tmp);
+            path[PATH_LEN-1] = '\0';
+        }
+        restCount++;
     }
+    return restCount;
+}
+
     
     return 0;
 }
